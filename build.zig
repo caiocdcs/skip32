6f7dc452ee92882629b28b5845ed71bdab62b235
Caio Silva <caio.cdcs@gmail.com>
2025-06-25 08:59:52 +0100

    build: update lib name

--- build.zig
+++ build.zig
@@ -15,7 +15,7 @@ pub fn build(b: *std.Build) void {
     // This creates a "module", which represents a collection of source files alongside
     // some compilation options, such as optimization mode and linked system libraries.
     // Every executable or library we compile will be based on one or more modules.
-    const lib_mod = b.createModule(.{
+    const lib_mod = b.addModule("skip32", .{
         // `root_source_file` is the Zig "entry point" of the module. If a module
         // only contains e.g. external object files, you can make this `null`.
         // In this case the main source file is merely a path, however, in more
@@ -25,9 +25,6 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    // Now, we will create a static library based on the module we created above.
-    // This creates a `std.Build.Step.Compile`, which is the build step responsible
-    // for actually invoking the compiler.
     const lib = b.addLibrary(.{
         .linkage = .static,
         .name = "skip32",
